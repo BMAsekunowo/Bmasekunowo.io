e864d45d3210e57f10474ae33f8a3c1ca47f9d1d
BMAsekunowo <bm.asekunowo@gmail.com>
2025-01-22 08:48:09 +0100

    section-4-done
    Section 4 model is finally done, all that is left is texts and correct images

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ document.addEventListener("scroll", function () {
       percentages.forEach(function (percentage) {
           percentage.style.animationPlayState = "running"; // Start percentage animation
       });
-  } else {
+  }
+  
+  else {
       // Pause animations when the section is out of view
       skillBars.forEach(function (skillClass) {
           var skillBar = document.querySelector(skillClass);
@@ -90,4 +92,54 @@ document.addEventListener("DOMContentLoaded", function () {
   percentages.forEach(function (percentage) {
       percentage.style.animationPlayState = "paused"; // Pause percentage animation
   });
+});
+
+
+/* Animation for section 4 */
+document.addEventListener("scroll", function () {
+    // Select the section and animated elements
+    var section = document.querySelector("#section-4");
+    var animatedElements = [
+        ".serv1",
+        ".serv2",
+        ".servcov1"
+    ];
+
+    // Navbar height and total height above
+    var navbarHeight = 80; // Height of the fixed navbar
+    var totalHeightAbove = 2900; // Height of the sections above this one
+
+    // Get the position of the section relative to the viewport
+    var sectionPosition = section.getBoundingClientRect().top;
+
+    // Calculate trigger point, considering the navbar height
+    var triggerPoint = window.innerHeight - navbarHeight;
+
+    // Trigger animations when the section is in focus
+    if (sectionPosition < triggerPoint && sectionPosition > -totalHeightAbove) {
+        animatedElements.forEach(function (className) {
+            var element = document.querySelector(className);
+            element.style.animationPlayState = "running"; // Start animation
+        });
+    } else {
+        // Pause animations when the section is out of view
+        animatedElements.forEach(function (className) {
+            var element = document.querySelector(className);
+            element.style.animationPlayState = "paused"; // Pause animation
+        });
+    }
+});
+
+// Ensure animations are paused on page load
+document.addEventListener("DOMContentLoaded", function () {
+    var animatedElements = [
+        ".serv1",
+        ".serv2",
+        ".servcov1"
+    ];
+
+    animatedElements.forEach(function (className) {
+        var element = document.querySelector(className);
+        element.style.animationPlayState = "paused"; // Pause animation initially
+    });
 });
